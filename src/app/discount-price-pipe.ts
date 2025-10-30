ecd3d38ea9073cd0263bd7ea5266109f93e8bc7c
ahmad <ahmad1zain10@gmail.com>
2025-10-30 23:57:50 +0300

    fix: make DiscountPricePipe standalone and remove stray tmp_* files; ignore future tmp_*

--- src/app/discount-price-pipe.ts
+++ src/app/discount-price-pipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'discountPrice'
+  name: 'discountPrice',
+  standalone: true,
 })
 export class DiscountPricePipe implements PipeTransform {
 
